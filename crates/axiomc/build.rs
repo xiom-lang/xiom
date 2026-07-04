52c6856f6ce60952b7d7de54720d5208194a3a51
Lefteris Notas <lefterisnotas@gmail.com>
2026-07-04 16:02:53 +0300

    Refactor: Remove deprecated AXIOM modules and related code
    - Deleted memory utilities module (axiom.mem)
    - Removed networking library module (axiom.net)
    - Eliminated numeric traits and operations module (axiom.num)
    - Removed OS library module (axiom.os)
    - Deleted path manipulation module (axiom.path)
    - Removed pointer utilities module (axiom.ptr)
    - Eliminated random number generation module (axiom.rand)
    - Deleted reference counting module (axiom.rc)
    - Removed runtime reflection module (axiom.reflect)
    - Eliminated regular expressions module (axiom.regex)
    - Deleted serialization library module (axiom.serialize)
    - Removed string library module (axiom.string)
    - Eliminated synchronization primitives module (axiom.sync)
    - Deleted test framework module (axiom.test)
    - Removed threading module (axiom.thread)
    - Eliminated time library module (axiom.time)
    - Deleted transformation script for code generation (transform_codegen.ps1)

--- crates/axiomc/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("../../resource/img/axiom-icon.ico");
-        res.compile().unwrap();
-    }
-}
